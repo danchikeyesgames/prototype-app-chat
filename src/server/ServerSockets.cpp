1913b4bfac08cb4d2e446fcd05e761ca6bb268a5
danchikeyesgames <danchikhol@gmail.com>
2022-10-28 21:43:16 +0300

    add check errors in API

--- src/server/ServerSockets.cpp
+++ src/server/ServerSockets.cpp
@@ -9,21 +9,34 @@ ServerSockets::ServerSockets(unsigned short len_qeue) : port(4888) {
 
     sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
     create_bind(sockfd, (sockaddr *) &address, sizeof(address));
-    listen(sockfd, len_qeue);
+    change_listen(sockfd, len_qeue);
 }
 
 int ServerSockets::wait_accept() {
     clientfd = accept(sockfd, NULL, NULL);
+    if (clientfd < 0) {
+        perror("accept: ");
+    }
 
     return clientfd;
 }
 
 int ServerSockets::server_send(const void *msg, int len, int flags) {
-    return send(clientfd, msg, len, flags);
+    ssize_t num = send(clientfd, msg, len, flags);
+    if (num == -1) {
+        perror("send: ");
+    }
+
+    return num; 
 }
 
 int ServerSockets::server_recv(void *buf, int len, int flags) {
-    return recv(clientfd, buf, len, flags);
+    ssize_t num = recv(clientfd, buf, len, flags);
+    if (num == -1) {
+        perror("recv: ");
+    }
+
+    return num;
 }
 
 /**
