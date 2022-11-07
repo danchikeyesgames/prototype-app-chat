7b436c0e0ff068f1fb70b32710006c593d51a51c
danchikeyesgames <danchikhol@gmail.com>
2022-11-08 02:30:31 +0300

    add Threadpool API

---
+++ include/Threadpool.hpp
@@ -0,0 +1,38 @@
+#ifndef __THREADPOOL_HPP__
+#define __THREADPOOL_HPP__
+
+#include <pthread.h>
+#include <unistd.h>
+
+void* main_work(void* arg);
+
+typedef void (*pthreadpool_work_t)(void* arg);
+
+class Threadpool {
+    friend void* main_work(void* arg);
+    
+    private:
+        struct pth_work;
+        typedef struct pth_work pth_work_t;
+        pth_work_t* first_message;
+        pth_work_t* last_message;
+        pthread_mutex_t work_mutex;
+        pthread_cond_t  work_cond;
+        pthread_cond_t  info_cond;
+        size_t  thread;
+        size_t  count;
+        bool    stop;
+
+    public:
+        Threadpool(size_t num = 2);
+        bool Add(pthreadpool_work_t func, void* arg);
+        void Wait();
+        ~Threadpool();
+
+    private:
+        pth_work_t* create_work(pthreadpool_work_t func, void* arg);
+        pth_work_t* get_worker();
+        void        destroy_work(pth_work_t* worker);
+};
+
+#endif          // __THREADPOOL_HPP__
---
+++ src/Threadpool.cpp
@@ -0,0 +1,165 @@
+#include "../include/Threadpool.hpp"
+
+struct Threadpool::pth_work {
+    pthreadpool_work_t  func;
+    void*               arg;
+    pth_work* next;
+};
+
+
+Threadpool::Threadpool(size_t num) {
+    pthread_t threads;
+
+    first_message = NULL;
+    last_message = NULL;
+
+    pthread_mutex_init(&work_mutex, NULL);
+    pthread_cond_init(&work_cond, NULL);
+    pthread_cond_init(&info_cond, NULL);
+
+    thread = num;
+    count = 0;
+    stop = false;
+
+    for (size_t i = 0; i < thread; ++i) {
+        pthread_create(&thread, NULL, main_work, (void *) this);
+        pthread_detach(thread);
+    }
+}
+
+Threadpool::~Threadpool() {
+    pth_work_t* tmp = NULL;
+    pth_work_t* del_worker = NULL;
+    
+    
+    pthread_mutex_lock(&work_mutex);
+    tmp = first_message;
+    while (tmp != NULL) {
+        del_worker = tmp;
+        tmp = tmp->next;
+        destroy_work(del_worker);
+    }
+
+    stop = true;
+    pthread_cond_broadcast(&work_cond);
+    pthread_mutex_unlock(&work_mutex);
+
+    Wait();
+
+    pthread_mutex_destroy(&work_mutex);
+    pthread_cond_destroy(&work_cond);
+    pthread_cond_destroy(&info_cond);
+
+}
+
+
+void Threadpool::Wait() {
+    pthread_mutex_lock(&work_mutex);
+
+    while (thread != 0 && count != 0) {
+        pthread_cond_wait(&info_cond, &work_mutex);
+    }
+
+    pthread_mutex_unlock(&work_mutex);
+}
+
+bool Threadpool::Add(pthreadpool_work_t func, void* arg) {
+    pth_work_t* worker = create_work(func, arg);
+
+    pthread_mutex_lock(&work_mutex);
+    if (worker == NULL)
+        return false;
+
+    if (first_message == NULL) {
+        first_message = worker;
+        last_message = worker;
+    } else {
+        last_message->next = worker;
+        last_message = worker;
+    }
+
+    pthread_cond_broadcast(&work_cond);
+    pthread_mutex_unlock(&work_mutex);
+
+    return true;
+}
+
+/**
+ * PRIVATE SEGMENT 
+ */
+
+Threadpool::pth_work_t* Threadpool::create_work(pthreadpool_work_t func, void* arg) {
+    pth_work_t* worker;
+
+    worker = new pth_work_t;
+    worker->func = func;
+    worker->arg  = arg;
+    worker->next = NULL;
+
+    return worker;
+}
+
+void* main_work(void* arg) {
+    Threadpool* pool = (Threadpool *) arg;
+    Threadpool::pth_work_t* work_node = NULL;
+    bool stop = false;
+
+    while (1) {
+        pthread_mutex_lock(&pool->work_mutex);
+
+        work_node = pool->first_message;
+        stop = pool->stop;
+        while (work_node == NULL && !stop) {
+            pthread_cond_wait(&pool->work_cond, &pool->work_mutex);
+        }
+
+        if (stop)
+            break;
+
+        work_node = pool->get_worker();
+        ++pool->count;
+        pthread_mutex_unlock(&pool->work_mutex);
+
+        if (work_node != NULL) {
+            work_node->func(work_node->arg);
+            pool->destroy_work(work_node);
+        }
+
+        pthread_mutex_lock(&pool->work_mutex);
+        --pool->count;
+        if (!stop && pool->count == 0 && pool->first_message == NULL)
+            pthread_cond_signal(&pool->info_cond);
+        pthread_mutex_unlock(&pool->work_mutex);
+
+    }
+
+    --pool->thread;
+    pthread_cond_signal(&pool->info_cond);
+    pthread_mutex_unlock(&pool->work_mutex);
+
+    return NULL;
+}
+
+Threadpool::pth_work_t* Threadpool::get_worker() {
+    pth_work_t* worker;
+    
+    if (first_message == NULL) {
+        return NULL;
+    }
+
+    worker = first_message;
+    first_message = first_message->next;
+
+    if (first_message == NULL) {
+        last_message = NULL;
+    }
+
+    return worker;
+}
+
+void Threadpool::destroy_work(pth_work_t* worker) {
+    if (worker == NULL)
+        return;
+
+    delete worker;
+}
