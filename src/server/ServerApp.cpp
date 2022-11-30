e37e67fe8857004970033b62946443b236fee4de
danchikeyesgames <danchikhol@gmail.com>
2022-11-30 17:10:04 +0300

    add thread to wait clients

--- src/server/ServerApp.cpp
+++ src/server/ServerApp.cpp
@@ -2,16 +2,19 @@
 
 #include "../../include/xchat/Server.hpp"
 
+static void* wait_clients(void* args);
 
 int main() {
     Server myserver = 4888;
     char buf[1024];
     void* ptr = NULL;
     uint32_t one, two;
+    pthread_t connect_clients;
 
+    pthread_create(&connect_clients, NULL, wait_clients, &myserver);
 
-    myserver.WaitClient();
-    myserver.WaitClient();
+    // myserver.WaitClient();
+    // myserver.WaitClient();
     
     while (1) {
         myserver.SelectClient();
@@ -20,4 +23,12 @@ int main() {
     
     myserver.CloseSocket();
     return 0;
+}
+
+static void* wait_clients(void* args) {
+    Server* serv = (Server *) args;
+
+    while (1) {
+        serv->WaitClient();
+    }
 }
